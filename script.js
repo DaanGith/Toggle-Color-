9f8e75c16e93b456ae3d64bbda4a71a637dc2348
Daan <96069569+DaanGith@users.noreply.github.com>
2022-08-16 22:23:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let toggleNavStatus = false; 
 
 //const rtnBtn = document.querySelector("rtn-btn");
+const getSidebar = document.querySelector(".nav-sidebar");
+const getSidebarUl = document.querySelector(".nav-sidebar ul");
+const getSidebarTitle = document.querySelector(".nav-sidebar span");
+const getSidebarLinks = document.querySelectorAll(".nav-sidebar a");
+
 const redBtn = document.querySelector("#red-btn");
 const orangeBtn = document.querySelector("#orange-btn");
 const purpleBtn = document.querySelector("#purple-btn");
@@ -8,18 +13,22 @@ const yellowBtn = document.querySelector("#yellow-btn");
 const bluedBtn = document.querySelector("#blue-btn");
 const greenBtn = document.querySelector("#green-btn");
 
+const menuBtn = document.querySelector(".btn-toggle-nav");
+const getBgColorElement = document.querySelectorAll(".bgcolor");
 const bodyElement = document.body;
 
 
 
-//Function onclick open en dicht
-let toggleNav = function() {
-    let getSidebar = document.querySelector(".nav-sidebar");
-    let getSidebarUl = document.querySelector(".nav-sidebar ul");
-    let getSidebarTitle = document.querySelector(".nav-sidebar span");
-    let getSidebarLinks = document.querySelectorAll(".nav-sidebar a");
+/*menuBtn.addEventListener("click" , function() {
+    showNav();
+});*/
+   
+//Function open en close
+//Open
 
-    if (toggleNavStatus === false) {
+    let showNav = () => {
+    
+        if (toggleNavStatus === false);
         getSidebarUl.style.visibility = "visible";
         getSidebar.style.width = "272px";
         getSidebarTitle.style.opacity = "0.5";
@@ -29,12 +38,14 @@ let toggleNav = function() {
             getSidebarLinks[i].style.opacity = "1";
         }
 
-        toggleNavStatus = true;
-
+        toggleNavStatus = true;  
     }
     
-    else if (toggleNavStatus === true) {
-        
+    //close 
+
+    let closeNav = () => {
+
+    if (toggleNavStatus === true); 
         getSidebar.style.width = "50px";
         getSidebarTitle.style.opacity = "0";
 
@@ -46,10 +57,8 @@ let toggleNav = function() {
         getSidebarUl.style.visibility = "hidden";
 
         toggleNavStatus = false;
-
     }
-}
-
+    
 
 //addEvent
 
@@ -57,28 +66,36 @@ let toggleNav = function() {
     document.body.style.background = "white";
 });*/
 
+menuBtn.addEventListener("onmouseenter" , function(){
+    showNav();
+});
+
+menuBtn.addEventListener("onmouseleave" , function() {
+    closeNav();
+});
+
 redBtn.addEventListener("click" , () =>  {
-    document.body.style.background = "red";
+    document.body.style.background = "red"; closeNav();
+
 });
 
 orangeBtn.addEventListener("click" , () =>  {
-    document.body.style.background = "orange";
+    document.body.style.background = "orange"; closeNav();
 });
 
 purpleBtn.addEventListener("click" , () =>  {
-    document.body.style.background = "purple";
+    document.body.style.background = "purple"; closeNav();
 });
 
 yellowBtn.addEventListener("click" , () =>  {
-    document.body.style.background = "yellow";
+    document.body.style.background = "yellow"; closeNav();
 });
 
 bluedBtn.addEventListener("click" , () =>  {
-    document.body.style.background = "blue";
+    document.body.style.background = "blue"; closeNav();
 });
 
 greenBtn.addEventListener("click" , () =>  {
-    document.body.style.background = "green";
+    document.body.style.background = "green"; closeNav();
 
 });
-
